40b092bd070970830a4cc6a151ffa9392e973d89
Chobol <55927070+Chobol@users.noreply.github.com>
2021-02-25 14:56:08 +0100

    Delete srcs directory

--- srcs/printingIDK/bytes_written.c
+++
@@ -1,9 +0,0 @@
-
-#include "printing.h"
-
-int32_t	print_bytes(t_format format, va_list *app, t_buffer *buffer)
-{
-	format = (t_format)format;
-	*va_arg(*app, int *) = buffer->bytes_written;
-	return (SUCCESS);
-}
--- srcs/utilitiesRobado/padd_value.c
+++
@@ -1,8 +0,0 @@
-
-#include "utils.h"
-
-void	padd_value(char *pad_char, int32_t padding, t_buffer *buffer)
-{
-	while (padding-- > 0)
-		buffered_print(pad_char, 1, buffer);
-}
